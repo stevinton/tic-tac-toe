5b109ffbcd01b040777dafbe3a579ee88308c8f7
stevinton <stevinton9@gmail.com>
2024-02-29 16:14:52 +0530

    third commit

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ let board = [
 ];
 
 let optionChoose = (val) => {
-    currentPlayer = val;
-    compPlayer = currentPlayer === "X" ? "O" : "X";
+    currentPlayer = val
     document.querySelector(".option").classList.add('hide');
     document.querySelector(".board").classList.remove('hide');
 };
@@ -20,17 +19,34 @@ const cellClicked = (row, col) => {
     board[row][col] = currentPlayer;
     const p = document.querySelector(`.place-${row}-${col}`);
     p.textContent = String(currentPlayer);
-    compChance();
+    if(!checkWin() && !board.flat().includes('')){
+        
+    }
+    currentPlayer = currentPlayer == "X"? "O" : "X";
 };
 
-const compChance = () => {
-    let crow, ccol;
-    do {
-        crow = Math.floor(Math.random() * 3);
-        ccol = Math.floor(Math.random() * 3);
-    } while (board[crow][ccol] !== '');
+const checkWin = () => {
+    for(let i = 0; i < 3 ; i++){
+        if(board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]){
+            alert("wins");
+        }
+    }
 
-    board[crow][ccol] = compPlayer;
-    const p = document.querySelector(`.place-${crow}-${ccol}`);
-    p.textContent = String(compPlayer);
-};
+    for (let j = 0; j < 3; j++){
+        if(board[0][j] !== '' && board[0][j] === board[1][j] && board[1][j] === board[2][j]){
+            alert("wins");
+        }
+    }
+
+    if(board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]){
+        alert("wins");
+    }
+    if(board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]){
+        alert("wins");
+    }
+
+    if(!board.flat().includes('')){
+        console.log("draw");
+    }
+    return true
+}
